91544ca7e5864c3c79f1fb5dd7c2cae955c4c857
jasonyop <yopjason@gmail.com>
2024-07-25 13:42:56 +0800

    add web app 3

---
+++ docker/webapp_v3/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+import mysql.connector
+import datetime
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = mysql.connector.connect(
+        host='mysql',
+        database='logs_db',
+        user='user',
+        password='password'
+    )
+    return conn
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS access_logs (id INT AUTO_INCREMENT PRIMARY KEY, access_time DATETIME)')
+    cursor.execute('INSERT INTO access_logs (access_time) VALUES (%s)', (datetime.datetime.now(),))
+    conn.commit()
+
+    cursor.execute('SELECT * FROM access_logs')
+    logs = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return render_template('index.html', logs=logs)
+
+app.run(host='0.0.0.0', port=5000)
